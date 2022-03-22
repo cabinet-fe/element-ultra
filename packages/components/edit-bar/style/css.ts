c7ca9bb64a155f932b2b3c233a16fbfcd9385155
whj <emailwen@qq.com>
2022-03-22 17:58:16 +0800

    feat: [ci]新增EditBar组件, 完善组件文档

---
+++ packages/components/edit-bar/style/css.ts
@@ -0,0 +1,7 @@
+import '@element-ultra/components/base/style/css'
+import '@element-ultra/theme-chalk/el-edit-bar.css'
+
+import '@element-ultra/components/icon/style/css'
+import '@element-ultra/components/scrollbar/style/css'
+import '@element-ultra/components/popconfirm/style/css'
+import '@element-ultra/components/button/style/css'
---
+++ play/src/edit-bar-play.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-edit-bar sortable @create="onCreate" @delete="onDelete" style="background-color: #fff;" :data="data">
+    <template #default="item">
+      <span>
+        {{ item.label }}
+      </span>
+    </template>
+  </el-edit-bar>
+</template>
+
+<script setup lang="ts">
+let data = $ref([
+  { label: 0, value: Date.now() }
+])
+
+const onCreate = () => {
+  data.push({ label: data.length, value: Date.now() })
+}
+
+const onDelete = (v: any) => {
+  data = data.filter(item => item.value !== v)
+}
+</script>
