8b9a33fe0b11c4a0f316592ef73a77b1badf8510
赵金鹏 <z15044356261@163.com>
2022-03-29 11:05:36 +0800

    chore: 引入样式

--- packages/components/text-editor/src/text-editor.vue
+++ packages/components/text-editor/src/text-editor.vue
@@ -1,65 +1,64 @@
 <template>
-  <div class="box">
-     <div id="toolbar-container" style="width: 100%"></div>
-     <div id="editor-container" style="height: 100%; width: 100%"></div>
+  <div class="box" style="width: 900px">
+    <div id="toolbar-container" style="width: 100%"></div>
+    <div id="editor-container" style="height: 100%; width: 100%"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import { createEditor, createToolbar,  } from '@wangeditor/editor'
-import type { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
+import { onMounted, ref } from "vue";
+import { createEditor, createToolbar } from "@wangeditor/editor";
+import type {
+  IDomEditor,
+  IEditorConfig,
+  IToolbarConfig,
+} from "@wangeditor/editor";
 
-const editorConfig: Partial<IEditorConfig> = {}
-editorConfig.placeholder = '请输入内容'
+const prop = defineProps<{
+  model: String;
+  disable: Boolean | any;
+}>();
+
+let emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+
+  (e: "update:onChange" , value: any): void
+}>();
+const editorConfig: Partial<IEditorConfig> = {};
+editorConfig.placeholder = "请输入内容";
+/** 是否禁用 */
+editorConfig.readOnly = prop.disable;
 editorConfig.onChange = (editor: IDomEditor) => {
   // 当编辑器选区、内容变化时，即触发
-  console.log('content', editor.children)
-  console.log('html', editor.getHtml())
-}
-
-const toolbarConfig: Partial<IToolbarConfig> = {
-    toolbarKeys: [
-        // 菜单 key
-        'headerSelect',
-
-        // 分割线
-        '|',
-
-        // 菜单 key
-        'bold', 'italic',
-
-        // 菜单组，包含多个菜单
-        {
-            key: 'group-more-style', // 必填，要以 group 开头
-            title: '更多样式', // 必填
-            iconSvg: '<svg>....</svg>', // 可选
-            menuKeys: ["through", "code", "clearStyle"] // 下级菜单 key ，必填
-        }
-    ]
-}
+  // console.log("content", editor.children);
+  // console.log("html", editor.getHtml());
+  emit("update:modelValue", editor.getHtml());
+  emit("update:onChange", editor.getHtml())
+};
 
 const init = () => {
-
   // 创建编辑器
   const editor = createEditor({
-    selector: '#editor-container',
+    selector: "#editor-container",
     config: editorConfig,
-    mode: 'default',
-  })
+    mode: "default",
+  });
+
+  const toolbarConfig: Partial<IToolbarConfig> = {
+    toolbarKeys: [...editor.getAllMenuKeys()],
+  };
 
   // 创建工具栏
-  const toolbar = createToolbar({
+  createToolbar({
     editor,
-    selector: '#toolbar-container',
-    mode: 'default'
-  })
-
-  console.log(editor.getAllMenuKeys(),1243)
-}
+    selector: "#toolbar-container",
+    mode: "default",
+    config: toolbarConfig,
+  });
+  // console.log(editor.getAllMenuKeys(), 1243);
+};
 
 onMounted(() => {
-  init()
-})
+  init();
+});
 </script>
-
