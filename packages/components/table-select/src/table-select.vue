6b313b612e4de78b6ca072228ff6db7bdae059c2
snowerskk <914637371@qq.com>
2022-04-13 11:36:52 +0800

    feat: table-select init

---
+++ packages/components/table-select/src/table-select.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="ns.b()">
+    {{modelValue}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useNamespace } from '@element-ultra/hooks'
+import { tableSelectProps } from './table-select'
+
+defineOptions({
+  name: 'ElTableSelect'
+})
+
+const props = defineProps(tableSelectProps)
+
+const { modelValue } = props
+
+const emit = defineEmits<{
+
+}>()
+
+const ns = useNamespace('table-select')
+
+
+defineExpose({
+
+})
+</script>
