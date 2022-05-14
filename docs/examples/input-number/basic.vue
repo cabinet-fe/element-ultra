27c9e7b8692dcf3509985ad85495a7c77085214d
whj <emailwen@qq.com>
2022-05-14 23:12:27 +0800

    docs: [ci]

--- docs/examples/input-number/basic.vue
+++ docs/examples/input-number/basic.vue
@@ -1,12 +1,16 @@
 <template>
   <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
+  <br/>
+  <el-input-number v-model="num" :min="1" money :max="10" @change="handleChange" />
+  <br/>
+  <el-input-number v-model="num" :min="1" :max="10" append="平方" @change="handleChange" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const num = ref(1)
-const handleChange = (value: number) => {
+const handleChange = (value?: number) => {
   console.log(value)
 }
 </script>
