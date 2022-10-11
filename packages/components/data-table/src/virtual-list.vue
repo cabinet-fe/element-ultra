32240019aa72f5ae2bab5a5e4fd65a50f467172d
whj <emailwen@qq.com>
2022-10-11 10:20:31 +0800

    chore: 杂项

--- packages/components/data-table/src/virtual-list.vue
+++ packages/components/data-table/src/virtual-list.vue
@@ -145,7 +145,7 @@ watch(
 let scroll = debounce((s: ScrollCtx) => {
   position.value = ~~((s.scrollTop - props.bufferHeight) / props.itemSize)
   position.value < 0 && (position.value = 0)
-}, 10)
+}, 20)
 
 /** 用来cancelIdleCallback */
 let idleId: number
